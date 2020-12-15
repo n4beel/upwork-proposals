646f4731d21aac94d0620e659b96b5e12465737c
n4beel <nabeel3353045805@hotmail.com>
2020-12-15 13:13:46 +0500

    name changed

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  githubConvertedToken:
+    "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
 
   // Your Github UserName
   githubUserName: "saadpasta",
@@ -11,22 +12,22 @@ export const config = {
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
   enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  OAuthClientID: "Iv1.9f40fd53257d4102",
+  OAuthSecret: "cc3db76ffe9848c92d83172807eececa79f0eb22",
 
   // Your Personal Blog Title
   title: "Upwork Proposals",
 
   // Your Personal Blog Subtitle
-  subtitle: "Technoverse.ai",
+  subtitle: "Machine Learning | Web & Mobile | UI/UX",
 
   // Header customization
   header: {
-    backgroundColor: '#f1f6f8', // can be a CSS gradient
-    backgroundColorDark: '#4C566A',
-    titleColor: '#ff5252',
-    titleColorDark: '#16a085',
-    subtitleColor: '#37474f',
-    subtitleColorDark: '#D8DEE9',
+    backgroundColor: "#f1f6f8", // can be a CSS gradient
+    backgroundColorDark: "#4C566A",
+    titleColor: "#ff5252",
+    titleColorDark: "#16a085",
+    subtitleColor: "#37474f",
+    subtitleColorDark: "#D8DEE9",
   },
 };
